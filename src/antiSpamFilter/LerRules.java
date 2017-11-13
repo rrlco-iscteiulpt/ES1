0c941eac271a4be7f5d9105ba4abe1f1cde5e6c6
Mariana Barros <mabsa@iscte-iul.pt>
2017-11-13 20:57:42 +0000

    Leitura ficheiro Rules bem

---
+++ src/antiSpamFilter/LerRules.java
@@ -0,0 +1,37 @@
+package antiSpamFilter;
+	
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.util.ArrayList;
+	import java.util.Scanner;
+
+	public class LerRules {
+
+		public static ArrayList<String> lerRules(){
+			ArrayList<String> rules= new ArrayList<String>();
+			
+			try{
+				File f= new File("rules.cf");
+				Scanner s= new Scanner(f);
+				
+				try{
+					while(s.hasNextLine()){
+						String line=s.nextLine();
+						rules.add(line);
+					}
+				}finally{
+					s.close();
+				}
+			}catch(FileNotFoundException e){
+				e.printStackTrace();
+			}
+			System.out.println(rules);
+			return rules;
+		}
+		
+		public static void main(String[] args) {
+			lerRules();
+		}
+	}
+
+
