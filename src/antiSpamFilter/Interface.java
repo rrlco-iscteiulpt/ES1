11a7023691a31fc3d0a887bce9cfe04a38ed93f6
Rodrigo_Cortesao <rrlco@iscte-iul.pt>
2017-11-15 13:24:00 +0000

    Interface update

--- src/antiSpamFilter/Interface.java
+++ src/antiSpamFilter/Interface.java
@@ -1,35 +1,23 @@
 package antiSpamFilter;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.util.ArrayList;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.WindowConstants;
-import javax.swing.border.Border;
+import javax.swing.JTable;
 
 public class Interface {
 
+private ArrayList <Table_object> lista_regras;
 
-
+	private JTable tabela;
 	private JFrame frame;
-	//
-	//
+	private Table_Model modelo_tabela;
+	
 	public Interface() {
+		lista_regras = LerRules.lerRules();
 		addFrameContent();
 	}
 
@@ -40,39 +28,26 @@ public class Interface {
 		frame.setSize(600, 500);
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		//panelCenterWest.add();
+		
+		modelo_tabela = new Table_Model();
+		
+		for(Table_object obj : lista_regras){
+			modelo_tabela.add_regras(obj);
+		}
+		
+		tabela = new JTable(modelo_tabela);
+		JScrollPane scroll_tabela = new JScrollPane (tabela);
+		panel.add(scroll_tabela);
 		frame.add(panel,BorderLayout.CENTER);
+		
 
-
-		JScrollPane scrollPane = new JScrollPane(panel);
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(50, 30, 300, 50);
 
 	}
-		//			for(int i = 0; i != width; i++){
-		//				for(int j =0; j!=height ; j++){
-		//					JLabel aux = new JLabel();
-		//					
-		//					int auxi = i;
-		//					int auxj = j;
-		//					
-		//					aux.addMouseListener(new MouseAdapter() {
-		//						public void mouseClicked(MouseEvent e){
-		//						System.out.println("janela: " + Integer.toString(auxi) + "," + Integer.toString(auxj));
-		//						}
-		//		
-		//					});
-		//					Border border = BorderFactory.createLineBorder(Color.BLACK,1);
-		//					aux.setBorder(border);
-		//					frame.add(aux);
-		//				}
-		//			}
-		//		
-		//		}
-		//		
+				
 		public void open() {
 			frame.setVisible(true);
 		}
@@ -84,4 +59,3 @@ public class Interface {
 	}
 
 }
-//
