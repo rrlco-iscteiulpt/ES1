720678f44f6b92917da23cb0cd7a051aa4de8e12
Rodrigo_Cortesao <rrlco@iscte-iul.pt>
2017-11-17 16:18:58 +0000

    Interface update - Painel Manual e inicio painel automático

--- src/antiSpamFilter/Interface.java
+++ src/antiSpamFilter/Interface.java
@@ -1,6 +1,10 @@
 package antiSpamFilter;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -11,16 +15,15 @@ import javax.swing.JTable;
 
 public class Interface {
 
-private ArrayList <Table_object> lista_regras;
-
+	private ArrayList <Table_object> lista_regras;
 	private JTable tabela;
 	private JFrame frame;
 	private Table_Model modelo_tabela;
-	private JPanel painel_sul;
-	
-	private JButton botao_gravar;
-	private JButton botao_avaliar;
-	
+	private Table_Model modelo_tabela2;
+	private JButton evaluate;
+	private JButton save;
+	private ArrayList<Integer> valores_pesos = new ArrayList<>();
+
 	public Interface() {
 		lista_regras = ReadRules.lerRules();
 		addFrameContent();
@@ -31,12 +34,15 @@ private ArrayList <Table_object> lista_regras;
 		frame = new JFrame("Filtro Anti-SPAM");
 		frame.setLocation(50,50);
 		frame.setSize(600, 500);
-		frame.setLayout(new BorderLayout());
+		frame.setLayout(new GridLayout(2,2));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		//panelCenterWest.add();
+		
+		
+		//------------------ MANUAL PANEL ----------------------
+		//Lista
+		JPanel listPanel = new JPanel();
+		listPanel.setLayout(new BorderLayout());
 		
 		modelo_tabela = new Table_Model();
 		
@@ -44,24 +50,56 @@ private ArrayList <Table_object> lista_regras;
 			modelo_tabela.add_regras(obj);
 		}
 		
+		
 		tabela = new JTable(modelo_tabela);
 		JScrollPane scroll_tabela = new JScrollPane (tabela);
-		panel.add(scroll_tabela);
-		frame.add(panel,BorderLayout.CENTER);
+		listPanel.add(scroll_tabela, BorderLayout.CENTER);
 		
-		painel_sul = new JPanel();
-		frame.add(painel_sul, BorderLayout.SOUTH);
+		//Buttons
+		JPanel buttonPanel = new JPanel();
+		evaluate = new JButton("Avaliar configuração");
 		
-		botao_avaliar = new JButton ("Avaliar");
-		botao_gravar = new JButton ("Gravar");
+		evaluate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+			}
+		});
 		
-		painel_sul.add(botao_avaliar);
-		painel_sul.add(botao_gravar);
+		save = new JButton("Gravar configuração manual");
 		
-
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+			}
+		});
+		
+		buttonPanel.add(evaluate, BorderLayout.CENTER);
+		buttonPanel.add(save, BorderLayout.SOUTH);
+		listPanel.add(buttonPanel, BorderLayout.EAST);
+	
+		
+		frame.add(listPanel);
+		
+		//------------------ AUTO PANEL-------------------
+				//Lista
+				JPanel listAUTOPanel = new JPanel();
+				listAUTOPanel.setLayout(new BorderLayout());
+				
+				modelo_tabela2 = new Table_Model();
+				
+				for(Table_object obj : lista_regras){
+					modelo_tabela2.add_regras(obj);
+				}
+				
+				tabela = new JTable(modelo_tabela2);
+				JScrollPane scroll_tabela2 = new JScrollPane (tabela);
+				listAUTOPanel.add(scroll_tabela2, BorderLayout.CENTER);
+				
 
 	}
-				
+	
 		public void open() {
 			frame.setVisible(true);
 		}
